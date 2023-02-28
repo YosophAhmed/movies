4d9761f75c4b5d59fb98136b7ad809090b9571e0
Yosoph Ahmed <devyosoph@gmail.com>
2023-02-28 02:27:11 +0200

    Latest Commit

--- lib/modules/movies/presentation/controller/movies_bloc.dart
+++ lib/modules/movies/presentation/controller/movies_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies/core/utils/enums.dart';
 import 'package:movies/modules/movies/domain/usecases/get_now_playing_movies_usecase.dart';
 import 'package:movies/modules/movies/presentation/controller/movies_states.dart';
 import 'movies_events.dart';
@@ -11,6 +12,25 @@ class MoviesBloc extends Bloc<MoviesEvents, MoviesStates> {
   ) : super(const MoviesStates()) {
     on<GetNowPlayingMoviesEvent>((event, emit) async {
       final result = await getNowPlayingMoviesUseCase.execute();
+      emit(
+        const MoviesStates(
+          nowPlayingState: RequestState.loaded,
+        ),
+      );
+      result.fold(
+        (l) => emit(
+          MoviesStates(
+            nowPlayingState: RequestState.error,
+            nowPlayingMessage: l.message,
+          ),
+        ),
+        (r) => emit(
+          MoviesStates(
+            nowPlayingState: RequestState.loaded,
+            nowPlayingMovies: r,
+          ),
+        ),
+      );
     });
   }
 }
--- lib/modules/movies/presentation/screens/movies_screen.dart
+++ lib/modules/movies/presentation/screens/movies_screen.dart
@@ -1,337 +1,124 @@
-import 'package:animate_do/animate_do.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shimmer/shimmer.dart';
+import 'package:movies/modules/movies/presentation/controller/movies_bloc.dart';
+import 'package:movies/modules/movies/presentation/controller/movies_events.dart';
 
-import '../../../../core/network/api_constants.dart';
-import '../../../../core/utils/dummy.dart';
+import '../../../../core/services/services_locator.dart';
+import '../components/now_playing_component.dart';
+import '../components/popular_component.dart';
+import '../components/top_rated_component.dart';
 
 class MoviesScreen extends StatelessWidget {
   const MoviesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        key: const Key(
-          'movieScrollView',
+    return BlocProvider(
+      create: (context) => MoviesBloc(
+        sl(),
+      )..add(
+          GetNowPlayingMoviesEvent(),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            FadeIn(
-              duration: const Duration(
-                milliseconds: 500,
-              ),
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: 400.0,
-                  viewportFraction: 1.0,
-                  onPageChanged: (index, reason) {},
+      child: Scaffold(
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          key: const Key(
+            'movieScrollView',
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const NowPlayingComponent(),
+              Container(
+                margin: const EdgeInsets.fromLTRB(
+                  16.0,
+                  24.0,
+                  16.0,
+                  8.0,
                 ),
-                items: moviesList.map(
-                  (item) {
-                    return GestureDetector(
-                      key: const Key(
-                        'openMovieMinimalDetail',
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Popular",
+                      style: GoogleFonts.poppins(
+                        fontSize: 19,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.15,
                       ),
+                    ),
+                    InkWell(
                       onTap: () {
-                        /// TODO : NAVIGATE TO MOVIE DETAILS
+                        /// TODO : NAVIGATION TO POPULAR SCREEN
                       },
-                      child: Stack(
-                        children: [
-                          ShaderMask(
-                            shaderCallback: (rect) {
-                              return const LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [
-                                  // fromLTRB
-                                  Colors.transparent,
-                                  Colors.black,
-                                  Colors.black,
-                                  Colors.transparent,
-                                ],
-                                stops: [
-                                  0,
-                                  0.3,
-                                  0.5,
-                                  1,
-                                ],
-                              ).createShader(
-                                Rect.fromLTRB(
-                                  0,
-                                  0,
-                                  rect.width,
-                                  rect.height,
-                                ),
-                              );
-                            },
-                            blendMode: BlendMode.dstIn,
-                            child: CachedNetworkImage(
-                              height: 560.0,
-                              imageUrl: ApiConstants.imageUrl(
-                                item.image,
-                              ),
-                              fit: BoxFit.cover,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: const [
+                            Text(
+                              'See More',
                             ),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                    bottom: 16.0,
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const Icon(
-                                        Icons.circle,
-                                        color: Colors.redAccent,
-                                        size: 16.0,
-                                      ),
-                                      const SizedBox(
-                                        width: 4.0,
-                                      ),
-                                      Text(
-                                        'Now Playing'.toUpperCase(),
-                                        style: const TextStyle(
-                                          fontSize: 16.0,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                    bottom: 16.0,
-                                  ),
-                                  child: Text(
-                                    item.title,
-                                    textAlign: TextAlign.center,
-                                    style: const TextStyle(
-                                      fontSize: 24,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ).toList(),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(
-                16.0,
-                24.0,
-                16.0,
-                8.0,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Popular",
-                    style: GoogleFonts.poppins(
-                      fontSize: 19,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.15,
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      /// TODO : NAVIGATION TO POPULAR SCREEN
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: const [
-                          Text(
-                            'See More',
-                          ),
-                          Icon(
-                            Icons.arrow_forward_ios,
-                            size: 16.0,
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            FadeIn(
-              duration: const Duration(
-                milliseconds: 500,
-              ),
-              child: SizedBox(
-                height: 170.0,
-                child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16.0,
-                  ),
-                  itemCount: moviesList.length,
-                  itemBuilder: (context, index) {
-                    final movie = moviesList[index];
-                    return Container(
-                      padding: const EdgeInsets.only(
-                        right: 8.0,
-                      ),
-                      child: InkWell(
-                        onTap: () {
-                          /// TODO : NAVIGATE TO  MOVIE DETAILS
-                        },
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(
-                              8.0,
-                            ),
-                          ),
-                          child: CachedNetworkImage(
-                            width: 120.0,
-                            fit: BoxFit.cover,
-                            imageUrl: ApiConstants.imageUrl(
-                              movie.image,
-                            ),
-                            placeholder: (context, url) => Shimmer.fromColors(
-                              baseColor: Colors.grey[850]!,
-                              highlightColor: Colors.grey[800]!,
-                              child: Container(
-                                height: 170.0,
-                                width: 120.0,
-                                decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(8.0),
-                                ),
-                              ),
-                            ),
-                            errorWidget: (context, url, error) => const Icon(
-                              Icons.error,
-                            ),
-                          ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 16.0,
+                            )
+                          ],
                         ),
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(
-                16.0,
-                24.0,
-                16.0,
-                8.0,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Top Rated",
-                    style: GoogleFonts.poppins(
-                      fontSize: 19,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.15,
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      /// TODO : NAVIGATION TO Top Rated Movies Screen
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(
-                        8.0,
-                      ),
-                      child: Row(
-                        children: const [
-                          Text(
-                            'See More',
-                          ),
-                          Icon(
-                            Icons.arrow_forward_ios,
-                            size: 16.0,
-                          )
-                        ],
+              const PopularComponent(),
+              Container(
+                margin: const EdgeInsets.fromLTRB(
+                  16.0,
+                  24.0,
+                  16.0,
+                  8.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Top Rated",
+                      style: GoogleFonts.poppins(
+                        fontSize: 19,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.15,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            FadeIn(
-              duration: const Duration(
-                milliseconds: 500,
-              ),
-              child: SizedBox(
-                height: 170.0,
-                child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  itemCount: moviesList.length,
-                  itemBuilder: (context, index) {
-                    final movie = moviesList[index];
-                    return Container(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: InkWell(
-                        onTap: () {
-                          /// TODO : NAVIGATE TO  MOVIE DETAILS
-                        },
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(
-                              8.0,
+                    InkWell(
+                      onTap: () {
+                        /// TODO : NAVIGATION TO Top Rated Movies Screen
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(
+                          8.0,
+                        ),
+                        child: Row(
+                          children: const [
+                            Text(
+                              'See More',
                             ),
-                          ),
-                          child: CachedNetworkImage(
-                            width: 120.0,
-                            fit: BoxFit.cover,
-                            imageUrl: ApiConstants.imageUrl(
-                              movie.image,
-                            ),
-                            placeholder: (context, url) => Shimmer.fromColors(
-                              baseColor: Colors.grey[850]!,
-                              highlightColor: Colors.grey[800]!,
-                              child: Container(
-                                height: 170.0,
-                                width: 120.0,
-                                decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(
-                                    8.0,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            errorWidget: (context, url, error) => const Icon(
-                              Icons.error,
-                            ),
-                          ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 16.0,
+                            )
+                          ],
                         ),
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 50.0,
-            ),
-          ],
+              const TopRatedComponent(),
+              const SizedBox(
+                height: 50.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
